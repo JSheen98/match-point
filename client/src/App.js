23b14b3a4dae3d9e361c21c109dab76f03356fcd
Kerby00 <119148777+Kerby00@users.noreply.github.com>
2023-05-20 22:03:34 -0600

    added event page and sport pages

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,12 @@ import { setContext } from '@apollo/client/link/context'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 // import Home from './pages/Home' 
 import Calendar from "./pages/Calendar";
+import Events from './pages/Events'
+import Basketball from './pages/sports/basketball';
+import Soccer from './pages/sports/soccer';
+import Baseball from './pages/sports/baseball';
+import Football from './pages/sports/football';
+import Other from './pages/sports/other';
 import Navbar from "./components/Navbar/Navbar";
 
 const httpLink = createHttpLink({
@@ -45,6 +51,12 @@ function App() {
           <Routes>
             {/* <Route path="/" element={<Home />} /> */}
             <Route path="/calendar" element={<Calendar />} />
+            <Route path="/events" element={<Events />} />
+            <Route path="/basketball" element={<Basketball />} />
+            <Route path="/soccer" element={<Soccer />} />
+            <Route path="/baseball" element={<Baseball />} />
+            <Route path="/football" element={<Football />} />
+            <Route path="/others" element={<Other />} />
           </Routes>
         </Router>
       </div>

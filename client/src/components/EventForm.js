d7fdf21c0cef538b94d23da67dbf71a9b0fb1113
dukanegolder <dukanegolder@gmail.com>
2023-05-22 19:09:32 -0600

    added css to forms

--- client/src/components/EventForm.js
+++ client/src/components/EventForm.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { Button, Form } from 'semantic-ui-react';
-import { useMutation } from '@apollo/client';
-import { ADD_EVENT } from '../utils/mutations';
+import React, { useState } from "react";
+import { Button, Form } from "semantic-ui-react";
+import { useMutation } from "@apollo/client";
+import { ADD_EVENT } from "../utils/mutations";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 // import DatePicker from 'react-date-picker';
@@ -13,94 +13,97 @@ import "react-datepicker/dist/react-datepicker.css";
 // will need to set a filter of some kind to prevent older events from rendering on homepage otherwise.
 // const minDate = new Date(date('Min date', new Date(currentDate)));
 
-
-
 const EventForm = () => {
-    const [formInput, setFormInput] = useState({name: '', sport: '', location: '', date: '', time: ''})
-    const [create, { error }] = useMutation(ADD_EVENT)
+  const [formInput, setFormInput] = useState({
+    name: "",
+    sport: "",
+    location: "",
+    date: "",
+    time: "",
+  });
+  const [create, { error }] = useMutation(ADD_EVENT);
 
-    const handleDateChange = (date) => {
-        console.log(date)
-        setFormInput({ ...formInput, date: date })
-    }
-    console.log(formInput)
+  const handleDateChange = (date) => {
+    console.log(date);
+    setFormInput({ ...formInput, date: date });
+  };
+  console.log(formInput);
 
-    const handleChange = (e) => {
-        const { name, value } = e.target
-        setFormInput({ ...formInput, [name]: value })
-    }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormInput({ ...formInput, [name]: value });
+  };
 
-    const handleFormSubmit = async (e) => {
-        e.preventDefault()
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
 
-        try {
-            const { data } = await create({
-                variables: { ...formInput }
-            })
+    try {
+      const { data } = await create({
+        variables: { ...formInput },
+      });
 
-            // Auth.login(data.create.token)
-        } catch (err) {
-            console.error(err)
-        }
-
-        setFormInput({
-            name: '',
-            sport: '',
-            location: '',
-            date: '',
-            time: ''
-        })
+      // Auth.login(data.create.token)
+    } catch (err) {
+      console.error(err);
     }
 
-    return (
-        <Form onSubmit={handleFormSubmit}>
-        <Form.Field>
-            <Form.Input
-                value={formInput.name}
-                onChange={handleChange}
-                type='text'
-                name='name'
-                placeholder='e.g. Basketball @ Rec'
-                label="Event Name"
-            />
-        </Form.Field>
+    setFormInput({
+      name: "",
+      sport: "",
+      location: "",
+      date: "",
+      time: "",
+    });
+  };
 
-        <Form.Field>
-            <Form.Input
-                value={formInput.sport}
-                onChange={handleChange}
-                type='text'
-                name='sport'
-                placeholder='e.g. Backetball'
-                label="sport"
-            />
-        </Form.Field>
+  return (
+    <Form onSubmit={handleFormSubmit}>
+      <Form.Field>
+        <Form.Input
+          value={formInput.name}
+          onChange={handleChange}
+          type="text"
+          name="name"
+          placeholder="e.g. Basketball @ Rec"
+          label="Event Name"
+        />
+      </Form.Field>
 
-        <Form.Field>
-            <Form.Input
-                value={formInput.location}
-                onChange={handleChange}
-                type='text'
-                name='location'
-                placeholder='e.g. 123 Address Rd'
-                label="location"
-            />
-        </Form.Field>  
-{/* For date, might need dropdown calendar. */}
-{/* TOOL: https://www.npmjs.com/package/react-semantic-ui-datepickers */}
-        
-        <Form.Group width="equals">
-            <DatePicker
-                selected={formInput.date} 
-                onChange={((date) => handleDateChange(date))}
-                showTimeSelect
-                dateFormat="Pp"
-            />
-        </Form.Group>
-      
+      <Form.Field>
+        <Form.Input
+          value={formInput.sport}
+          onChange={handleChange}
+          type="text"
+          name="sport"
+          placeholder="e.g. Basketball"
+          label="sport"
+        />
+      </Form.Field>
 
-    {/* //in case we need to include the code that snapshots the user's contact info*/}
-        {/* <Form.Field>
+      <Form.Field>
+        <Form.Input
+          value={formInput.location}
+          onChange={handleChange}
+          type="text"
+          name="location"
+          placeholder="e.g. 123 Address Rd"
+          label="location"
+        />
+      </Form.Field>
+      {/* For date, might need dropdown calendar. */}
+      {/* TOOL: https://www.npmjs.com/package/react-semantic-ui-datepickers */}
+
+      <Form.Group width="equals">
+        <DatePicker
+          selected={formInput.date}
+          onChange={(date) => handleDateChange(date)}
+          showTimeSelect
+          dateFormat="Pp"
+        />
+      </Form.Group>
+
+      {/* //in case we need to include the code that snapshots the user's contact info*/}
+      {/* <Form.Field>
             <Form.Input
                 value={formInput.username}
                 onChange={handleChange}
@@ -130,9 +133,9 @@ const EventForm = () => {
                 label="Phone Number"
             />
         </Form.Field> */}
-        <Button type='submit'>Submit</Button>
+      <Button type="submit">Submit</Button>
     </Form>
-    )
-}
+  );
+};
 
 export default EventForm;

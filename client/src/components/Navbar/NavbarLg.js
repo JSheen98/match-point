c97ecb771c07554fddc52ac3d779981cfde30827
Saul473 <118771655+Saul473@users.noreply.github.com>
2023-05-22 18:37:43 -0600

    ppp

--- client/src/components/Navbar/NavbarLg.js
+++ client/src/components/Navbar/NavbarLg.js
@@ -1,5 +1,5 @@
 
-import React { useState } from 'react';
+import React, { useState } from 'react';
 import { Menu, Segment } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import Auth from '../../utils/auth'
